768fbc2e5f44a98171e7985202845d03e32ee988
Riham <rihammohareb6@gmail.com>
2024-08-27 18:38:14 +0300

    Add project files.

---
+++ Q3DotNetAssuit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Q3DotNetAssuit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
